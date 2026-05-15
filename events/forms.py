8ac71e47a93b3288cdb8315f6ccfca8441d59eab
Kasen Fudge <kasenfudge06@gmail.com>
2026-05-15 16:42:36 -0500

    Had to fix the events page

--- events/forms.py
+++ events/forms.py
@@ -3,6 +3,13 @@ from django import forms
 from .models import EventAttendee
 
 class EventRegistrationForm(forms.ModelForm):
+    # Override the arrival_time field to explicitly accept AM/PM formatting
+    arrival_time = forms.TimeField(
+        input_formats=['%I:%M %p', '%I:%M%p', '%H:%M'],
+        widget=forms.TextInput(attrs={'placeholder': 'e.g., 09:30 AM'}),
+        label="Arrival Time"
+    )
+
     PAYMENT_CHOICES = [
         ("online", "Pay online"),
         ("in_person", "Pay in person")
@@ -16,11 +23,12 @@ class EventRegistrationForm(forms.ModelForm):
 
     class Meta:
         model = EventAttendee
-        fields = ["arrival_time"]
+        # Add the fields to be processed upon submission
+        fields = ["arrival_time", "payment_method"]
 
     def __init__(self, *args, event=None, user=None, **kwargs):
         self.event = event
         self.user = user
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
     
     # Could use clean to validate arrival time is within bounds of the event.
