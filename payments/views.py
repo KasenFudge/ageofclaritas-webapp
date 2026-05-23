f9c8121cd816f30d38dc6cae53b00dc1c1143393
Kasen Fudge <kasenfudge06@gmail.com>
2026-05-22 22:06:41 -0500

    Another quick view bug

--- payments/views.py
+++ payments/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 @login_required
-def payment_start_view(request):
+def payment_start_view(request, registration_id):
     # Just a fast dummy view to catch the landing redirect
     return render(request, "payments/payment_start.html", {
         "title": "Online Payment Placeholder"
