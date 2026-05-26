000253e849585989f4d8e1cc7d771b1bd70c3d0d
Kasen Fudge <kasenfudge06@gmail.com>
2026-05-25 22:44:49 -0500

    Working through a fix?

--- payments/urls.py
+++ payments/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = "payments"
 urlpatterns = [
     # Where the form POSTs to start checkout
-    path("checkout/", views.payment_start_view, name="payment_start_view"),
+    path("checkout/", views.payment_start_view, name="payment_start"),
     
     # The backend destination path
     path("webhook/", views.stripe_webhook_view, name="stripe_webhook"),
     
     # The user landing pages after Stripe bounces them back
-    path("success/", views.payment_success_view, name="payment_success_view"),
-    path("cancel/", views.payment_cancel_view, name="payment_cancel_view"),
+    path("success/", views.payment_success_view, name="payment_success"),
+    path("cancel/", views.payment_cancel_view, name="payment_cancel"),
 ]
