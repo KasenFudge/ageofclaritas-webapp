40f194f2f5d41730c1d0de9739a034b111d5365a
Kasen Fudge <kasenfudge06@gmail.com>
2026-05-25 22:40:47 -0500

    Fixing some event pages

--- events/urls.py
+++ events/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("<slug:slug>/", views.EventDetailView.as_view(), name="event_detail"),
-    path("<slug:slug>/register/", views.event_registration_view, name="event_registration"),
+    path("<slug:slug>/", views.EventDetailView.as_view(), name="detail"),
+    path("<slug:slug>/register/", views.event_registration_view, name="register"),
 ]
