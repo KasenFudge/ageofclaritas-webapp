7cf2a6a3b2e369cabaa131e78058d10c7028cef7
Kasen Fudge <kasenfudge06@gmail.com>
2026-06-16 21:48:42 +0000

    Slight Fix to urls

--- payments/urls.py
+++ payments/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = "payments"
 urlpatterns = [
     path("checkout/", views.checkout_page, name="checkout"),
-    path("webhook/", views.stripe_webhook_handler, name="stripe_webhook"),
+    path("webhook/", views.stripe_webhook, name="stripe_webhook"),
     path("success/", TemplateView.as_view(template_name="payments/success.html"), name="payment_success"),
 ]
