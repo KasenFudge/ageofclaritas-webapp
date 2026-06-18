b8f9192380d8eae987d45edc56318b40506ca675
Kasen Fudge <kasenfudge06@gmail.com>
2026-06-17 20:49:54 -0500

    Trying to improve Account Creation 2

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,11 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class": "form-control"})
+
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + (
